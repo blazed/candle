9a2c541611127a489a3504fb2aca5d1f0f1fd6f2
Boberg <1823919+blazed@users.noreply.github.com>
2025-09-30 12:22:18 +0200

    use gofumpt for Go

--- modules/nixvim/plugins/conform/default.nix
+++ modules/nixvim/plugins/conform/default.nix
@@ -83,7 +83,7 @@
           fish = [ "fish_indent" ];
           fsharp = [ "fantomas" ];
           gdscript = [ "gdformat" ];
-          go = [ "golines" ];
+          go = [ "gofumpt" ];
           java = [ "google-java-format" ];
           javascript = {
             __unkeyed-1 = "prettierd";
@@ -156,7 +156,7 @@
           deno_fmt.command = lib.getExe pkgs.deno;
           fantomas.command = lib.getExe pkgs.fantomas;
           gdformat.command = lib.getExe' pkgs.gdtoolkit_4 "gdformat";
-          golines.command = lib.getExe pkgs.golines;
+          gofumpt.command = lib.getExe pkgs.gofumpt;
           google-java-format.command = lib.getExe pkgs.google-java-format;
           isort.command = lib.getExe pkgs.isort;
           jq.command = lib.getExe pkgs.jq;
